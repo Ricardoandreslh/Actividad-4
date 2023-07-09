26d61e956218bdd02984501e336ac8bb2da8ac21
Ricardoandreslh <ricardoandresloaiza@gmail.com>
2023-07-08 21:34:22 -0400

    Commit del buscador listo, faltan algunas cosas y el index, falta css

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 const searchInput = document.getElementById('searchInput');
 const searchResults = document.getElementById('searchResults');
 
-
 const debounceSearch = _.debounce(() => {
     const searchTerm = searchInput.value.trim();
 
- 
     if (searchTerm.length < 3) {
         searchResults.innerHTML = 'Please enter at least 3 characters';
         return;
@@ -13,24 +11,29 @@ const debounceSearch = _.debounce(() => {
 
     searchResults.innerHTML = '';
 
-    fetch(`https://api.github.com/search/users?q=${searchTerm}`)
-        .then(response => response.json())
-        .then(data => {
-            const users = data.items.slice(0, 3);
+    const apiUrl = `https://api.github.com/search/users?q=${searchTerm}`;
+    $.ajax({
+        url: apiUrl,
+        method: 'GET',
+        success: function(response) {
+            const users = response.items.slice(0, 3);
             users.forEach(user => {
                 const userDiv = document.createElement('div');
                 userDiv.innerHTML = `
                     <img src="${user.avatar_url}" alt="User Profile Picture" />
-                    <p>Name | Nombre:  ${user.login}</p>
+                    <p>Name | Nombre: ${user.login}</p>
                     <p>Github Name | Github Nombre: ${user.login}</p>
-                    <p>Company | Compañia:  ${user.company ? user.company : 'N/A'}</p>
+                    <p>Company | Compañia: ${user.company ? user.company : 'N/A'}</p>
                     <p>Repositorios: ${user.public_repos}</p>
                 `;
                 searchResults.appendChild(userDiv);
             });
-        })
-        .catch(error => {
+        },
+        error: function(error) {
             searchResults.innerHTML = 'Un error ha ocurrido haciendo fetch';
             console.error(error);
-        });
+        }
+    });
 }, 500);
+
+searchInput.addEventListener('keyup', debounceSearch);
